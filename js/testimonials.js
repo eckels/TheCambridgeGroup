1bd95979fc9bcfd5f78ea324f24d53174661c480
Evan Eckels <eveckels@gmail.com>
2020-02-10 16:39:15 -0600

    changed images and expertise items

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,5 +1,18 @@
 
 var maxHeight = 0;
+$( document ).ready(function() {
+    $(".testimony").each(function() {
+        var temp = $(this).height();
+        if (maxHeight < temp) {
+            maxHeight = temp;
+        }
+    });
+    if ($(window).width() > 750) {
+        $('.testimony').css('height', maxHeight);
+        $('.bottom').css('position', 'absolute');
+    }
+});
+
 $(".testimony").each(function() {
     var temp = $(this).height();
     if (maxHeight < temp) {
